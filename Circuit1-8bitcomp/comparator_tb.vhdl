194e4c97d5262aae1134f99155e7d9b87a6ed783
AshutoshGupta99 <35488690+AshutoshGupta99@users.noreply.github.com>
2021-03-01 22:58:16 +0530

    Update comparator_tb.vhdl

--- Circuit1-8bitcomp/comparator_tb.vhdl
+++ Circuit1-8bitcomp/comparator_tb.vhdl
@@ -2,7 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 
---this is how entity for your test bench code has to be declared.
 entity testbench is
 end testbench;
 
@@ -33,16 +32,16 @@ begin
              
    stim_proc: process
    begin
- num1<="00100000";  --num1 =2
-num2<="01010000"; --num2 =9
+ num1<="00100000";  
+num2<="01010000"; 
 wait for 2 ns;
 
-num1<="01010000";  --num1 =9
-num2<="00100000";   --num2 =2
+num1<="01010000";  
+num2<="00100000";   
 wait for 2 ns;
 
-num1<="01100000";  --num1 =10
-num2<="01100000";  --num2 =10
+num1<="01100000";  
+num2<="01100000";  
 --more input combinations can be given here.
 wait;  
    	
